1fa8677700335d86c805a0fedf6a3cc41686d50d
petterigit <p-makela@outlook.com>
2018-12-22 18:19:18 +0200

    menu commit

--- app/src/main/java/com/example/petteri/nuorisotapahtuma/QuestActivity.java
+++ app/src/main/java/com/example/petteri/nuorisotapahtuma/QuestActivity.java
@@ -5,14 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.TextView;
-
-import java.util.ArrayList;
 
 public class QuestActivity extends AppCompatActivity {
 
@@ -26,6 +27,7 @@ public class QuestActivity extends AppCompatActivity {
 
     PopupWindow infoPopup;
     Button infoButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +43,8 @@ public class QuestActivity extends AppCompatActivity {
         infoPopup = new PopupWindow(this);
 
 
+
+
         eventlist = EventList.getInstance();
         eventlist.createEvent("Junnukertsi", "18:00", "22:00", "Ahjola");
 
@@ -48,7 +52,32 @@ public class QuestActivity extends AppCompatActivity {
         beginsTextView.setText(eventlist.getEvent(0).getBegins());
         endsTextView.setText(eventlist.getEvent(0).getEnds());
         placeTextView.setText(eventlist.getEvent(0).getPlace());
+
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.menu_id1:
+                System.out.println("Logout");
+                return true;
+            case R.id.menu_id2:
+                System.out.println("Settings");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 
     public void launchFeedback(View quest) {
         Intent intent = new Intent(this, FeedbackActivity.class);
